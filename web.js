d243e36fdc322d50d80396e2a24aa532b031e903
Sanghyun Yuk <sang.yuk@gmail.com>
2013-07-07 04:24:09 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -3,7 +3,9 @@ var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+var fs = require ('fs');
+var buffer = new buffer(256);
+  response.send(buffer.toString('utf8', fs.readFileSync('index.html'))); 
 });
 
 var port = process.env.PORT || 5000;
